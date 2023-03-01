6897abc0602d4c4fc8c8783df65edc6fcc107ff1
Annashevchuk <sevcukanna960@gmail.com>
2023-03-01 05:11:35 +0200

    hm ex_3

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+using namespace std;
+
+int main()
+{
+	system("chcp 1251");
+	system("cls");
+	cout << " Продам цуценят.\n Порода-ротвейлер.\n Тел:222-222-222 \n |222||222||222| \n |222||222||222| \n |222||222||222| "  << endl;
+	// Bjarne Stroustrup 
+
+	return 0;
+
+}
