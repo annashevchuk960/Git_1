b63749ef299e0cef65e4dad99ec8974e9b6f0c5f
Annashevchuk <sevcukanna960@gmail.com>
2023-03-01 04:38:23 +0200

    ex_3

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+using namespace std;
+
+int main()
+{
+
+	cout << " To be, or not to be: that is the question: \n Whether 'tis nobler in the mind to suffer \n The slings and arrows of outrageous fortune,\n Or to take arms against a sea of troubles, \n And by opposing end them ? \n William Shakespeare "  << endl;
+
+
+	return 0;
+}
