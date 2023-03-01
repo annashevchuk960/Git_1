4cf7e587726d243734d927bd3871bf2cf0385d46
Annashevchuk <sevcukanna960@gmail.com>
2023-03-01 05:05:38 +0200

    ex1

--- main.cpp
+++ main.cpp
@@ -7,4 +7,5 @@ int main()
 	cout << " It's harder and harder to get you to listen,\n More I get through the gears,\n Incapable of making alright decisions,\n And having bad ideas.\n   Arctic Monkeys" << endl;
 
 	return 0;
+
 }
