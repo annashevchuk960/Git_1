6599bc535b26c7e1d809ff1a9f8ea50233a2af50
Annashevchuk <sevcukanna960@gmail.com>
2023-03-01 05:04:05 +0200

    hm ex_3

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+using namespace std;
+
+int main()
+{
+
+	cout << "\tevery\n\t\t hunter \n\t\t\t wants\n\t\t\t\t to know \n\t\t\t\t\twhere does \n\t\t\t\t\t\tpheasant \n\t\t\t\t\t\t\tsit" << endl;
+
+	return 0;
+
+}
