7f400f27597147db9c5cadf67dae7aaef867b403
Annashevchuk <sevcukanna960@gmail.com>
2023-03-12 18:38:49 +0200

    1

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream> 
+#include <ctime> 
+
+using namespace std;
+int main() {
+    std::cout << "Input a: ";
+    int a;
+    std::cin >> a;
+
+    std::cout << "Input b: ";
+    int b;
+    std::cin >> b;
+    summ = 0
+
+    while (a <= b) std::cout << a++ << ' ';
+    std::cout << std::endl;
+    return 0;
+}
