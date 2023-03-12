67e2e5b563fb77925218e03917b96d609e34c877
Annashevchuk <sevcukanna960@gmail.com>
2023-03-12 16:48:32 +0200

    ex_1

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+#include <ctime> 
+
+using namespace std; 
+int main() {
+	int counter;
+	std::cout << "Input n: ";
+	int n;
+	std::cin >> n;
+	for (int counter = 0; counter <= n; counter++)
+		{
+			cout << "" << counter;
+		}
+	return 0;
+}
