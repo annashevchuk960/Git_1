61c588f0955e66b119a716b2adfca1254649e6f9
Annashevchuk <sevcukanna960@gmail.com>
2023-03-16 17:33:56 +0200

    ex_4

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void cube(int n);
+
+int main()
+{
+    int n;
+    cout << "Number in cube: ";
+    cin >> n;
+    cout << "Cube  " << n << " = " ;
+    cube(n);
+    cout << endl;
+    return 0;
+}
+
+void cube(int n)
+{
+    for (int i = 1; i * i * i <= n; i++)
+    {
+        if (i * i * i == n)
+        {
+            cout << i << " ";
+        }
+    }
+}
