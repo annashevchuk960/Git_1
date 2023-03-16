eccc40fba525c2ae961ce0a965456a140de67dcf
Annashevchuk <sevcukanna960@gmail.com>
2023-03-16 15:50:03 +0200

    1

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+void rectangle(int name);
+double Sum(double x, double y);
+int main()
+{
+    int hight = ;
+    rectangle(hight);
+    cout << Sum(3, 4) << endl;
+    return 0;
+}
+void rectangle(int hight)
+{
+    cout << "Hello, " << rectangle << "!" << endl;
+}
+double Sum(double x, double y)
+{
+    return x + y;
+}
