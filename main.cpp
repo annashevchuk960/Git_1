8aba39da79d160ea928c6597af4630b1bf66f8db
Annashevchuk <sevcukanna960@gmail.com>
2023-03-19 21:54:15 +0200

    ex_4

--- main.cpp
+++ main.cpp
@@ -1,27 +1,43 @@
 #include <iostream>
-#include <ctime> 
-using namespace std; 
-int main() 
-{ 
-	srand(time(0)); 
-	const int size = 5; 
-	int sum = 0; 
-	int perimetr[size]; 
-	for (size_t i = 0; 
-		i < size; i++) 
-	{ 
-		cout << "side[" << i << "] = "; 
-		cin >> perimetr[i]; 
-	} 
-	for (size_t i = 0; 
-		i < size; i++) 
-	{
-		cout << perimetr[i] << "\t"; 
-	}
-	for (int i = 0; i < size; i++) 
-	{ 
-		if (perimetr[i] > 0) sum += perimetr[i];
-	} 
-	cout << "Sum = " << sum << "\n\n"; 
-	return 0; 
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    srand(time(0));
+    const int size = 12;
+    int sum = 0;
+    int pributok[size];
+    int max = 0, min = 0;
+    int max_month = 1, min_month = 1;
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << "month[" << i + 1 << "] = ";
+        cin >> pributok[i];
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << pributok[i] << "\t";
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        if (pributok[i] > 0) {
+            sum += pributok[i];
+        }
+        if (pributok[i] > max) {
+            max = pributok[i];
+            max_month = i + 1;
+        }
+        if (pributok[i] < min || i == 0) {
+            min = pributok[i];
+            min_month = i + 1;
+        }
+    }
+    cout << "Sum = " << sum << "\n\n";
+    cout << "Max pributok is " << max << " in month " << max_month << endl;
+    cout << "Min pributok is " << min << " in month " << min_month << endl;
+    return 0;
 }
