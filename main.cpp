37d2cba8bc8d9eaa26f878500e1d34ebaaa6c4bb
Annashevchuk <sevcukanna960@gmail.com>
2023-03-19 14:08:31 +0200

    ex_3

--- main.cpp
+++ main.cpp
@@ -1,21 +1,27 @@
-#include <iostream> 
+#include <iostream>
 #include <ctime> 
-using namespace std;
-
-int main()
-{
-    srand(time(0));
-    const int size = 6;
-    int numbers[size];
-    for (size_t i = 0; i < size; i++)
-    {
-        cout << "number[" << i << "] = ";
-        cin >> numbers[i];
-    }
-    cout << endl; 
-    for (int i = size - 1; i >= 0; i--) {
-        cout << numbers[i] << "\t";
-    }
-    cout << endl; 
-    return 0;
+using namespace std; 
+int main() 
+{ 
+	srand(time(0)); 
+	const int size = 5; 
+	int sum = 0; 
+	int perimetr[size]; 
+	for (size_t i = 0; 
+		i < size; i++) 
+	{ 
+		cout << "side[" << i << "] = "; 
+		cin >> perimetr[i]; 
+	} 
+	for (size_t i = 0; 
+		i < size; i++) 
+	{
+		cout << perimetr[i] << "\t"; 
+	}
+	for (int i = 0; i < size; i++) 
+	{ 
+		if (perimetr[i] > 0) sum += perimetr[i];
+	} 
+	cout << "Sum = " << sum << "\n\n"; 
+	return 0; 
 }
